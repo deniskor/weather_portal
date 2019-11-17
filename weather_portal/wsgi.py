006d85ebc31a49d6db0032e53d6461672b80cfe1
deniskor <denking94@gmail.com>
2019-11-17 18:24:20 +0200

    heroku deploy

--- weather_portal/wsgi.py
+++ weather_portal/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for weater_portal project.
+WSGI config for weather_portal project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -13,6 +13,6 @@ from django.core.wsgi import get_wsgi_application
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '../.env')
 load_dotenv(dotenv_path)
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weater_portal.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'weather_portal.settings')
 
 application = get_wsgi_application()
